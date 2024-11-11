8ba408e2bb65a39d5279532e9af1ec6b2b58e471
theavalanch <jasonfreakinevans11@gmail.com>
2024-11-11 11:17:48 +0100

    toyin

--- database.py
+++ database.py
@@ -25,17 +25,17 @@ email = 'maya12@gmail.com'
 # cursor.execute("INSERT INTO users (name, age, email) VALUES (?, ?)", (name,  email))
 
 # Update a row of data 
-cursor.execute("UPDATE users SET age = ? WHERE id = ?", (12,4))
+cursor.execute("UPDATE users SET name = ? WHERE id = ?", ("Toyin",3))
 cursor.execute("UPDATE users SET age = NULL WHERE id = 1")
 
 # Delete a row of data
 cursor.execute("DELETE FROM users  WHERE email = 'john@gmail.com' ")
 
 # Insert a column of data
-cursor.execute("ALTER TABLE users ADD COLUMN gender TEXT")
+# cursor.execute("ALTER TABLE users ADD COLUMN gender TEXT")
 
 # Remove a column of data
-cursor.execute("ALTER TABLE users DROP COLUMN gender")
+# cursor.execute("ALTER TABLE users DROP COLUMN gender")
 
 # Select all data
 cursor.execute("SELECT * FROM users ORDER BY id DESC")
